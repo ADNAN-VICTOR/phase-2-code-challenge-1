be5f0f9aaeb55394fc1ae0b5abf94282458d3b77
adnan <adnan.victor@student.moringaschool.com>
2024-05-02 09:22:28 +0300

    comments

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -57,14 +57,21 @@ const App = () => {
   ]);
   const [searchTerm, setSearchTerm] = useState('');
 
+  //The addTransaction function takes a transaction object as a parameter and 
+  //updates the transactions state variable by spreading the existing transactions and 
+  //adding the new transaction to the array.
   const addTransaction = (transaction) => {
     setTransactions([...transactions, transaction]);
   };
-
+//The handleSearch function takes a term parameter and 
+//updates the searchTerm state variable with the entered search term.
   const handleSearch = (term) => {
     setSearchTerm(term);
   };
 
+
+  //This uses the filter method to iterate over the transactions array and checks if the lowercase description of 
+  //each transaction includes the lowercase search term.
   const filteredTransactions = transactions.filter(transaction =>
     transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
